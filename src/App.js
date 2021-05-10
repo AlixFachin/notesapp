a46d6f054d9260d52f18f7700cb2ddf9777fa494
Alix Fachin <alix.fachin@me.com>
2021-05-10 10:15:19 +0900

    first iteration of front-end

--- src/App.js
+++ src/App.js
@@ -1,23 +1,29 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+       <h1>Welcome to Blu Note </h1>
       </header>
+      <div id="mainContainer">
+        <side>
+          <div className="buttonPanel">
+            <button>Add Note </button>
+            <button>Delete Note</button>
+          </div>
+          <div id="noteList">
+          </div>
+        </side>
+        <main>
+          <h2> Note title / summary </h2>
+          <p> Main Note content </p>
+          <div className="buttonPanel">
+            <button> Save Note </button>
+            <button> Delete Note </button>
+          </div>
+        </main>
+      </div>
     </div>
   );
 }
